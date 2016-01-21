979a464bafaa11229e1bff83d4700155fe35ce1b
jess-of-the-woods <what.the.dickenss@gmail.com>
2016-01-21 18:47:36 +1300

    add index.html, main.css, racer.js, normalize.css

--- scripts/racer.js
+++ scripts/racer.js
@@ -2,13 +2,26 @@ $(document).ready(function() {
   var player1Move = 0;
   var player2Move = 0;
   var trackLength = 0;
-trackLength = prompt('Please type a number between 10 & 40 to choose your track length..');
+
+function trackLengthSet() {
+  trackLength = prompt("Please type a number between 10 & 40 to choose your track length..");
   if (trackLength < 10) {
-    trackLength = 10;
+    trackLength = 10
   };
-  else if (trackLength > 40) {
-    trackLength = 40;
+
+  for (i = 1; i <= trackLength; i++){
+    $("#player1_strip").append("<td>");
+    $("#player2_strip").append("<td>");
   };
+
+ // console.log(trackLength);
+};
+trackLengthSet();
+
+
+alert("Player 1 use 'q' to move, Player 2 use 'p'.");
+
+
 
 
 
