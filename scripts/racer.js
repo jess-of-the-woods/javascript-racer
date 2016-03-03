926c4b13a5a9e8524159295195cabcb5301328c3
jess-of-the-woods <what.the.dickenss@gmail.com>
2016-03-03 14:21:20 +1300

    clean up code, extra comments, remove unnecessary semicolons, spacing etc, add indentation

--- scripts/racer.js
+++ scripts/racer.js
@@ -1,81 +1,67 @@
-$(document).ready(function() {
-  var player1Move = 0;
-  var player2Move = 0;
-  var trackLength = 0;
+$(document).ready(function () {
+  var player1Move = 0
+  var player2Move = 0
+  var trackLength = 0
 
-//this section sets the length of the track from user input
-function setTrackLength() {
-  trackLength = prompt("Please type a number between 10 & 40 to choose your track length..");
-  if (trackLength < 10) {
-    trackLength = 10
-  };
+// this section sets the length of the track from user input
+  function setTrackLength () {
+    trackLength = prompt("Please type a number between 10 & 40 to choose your track length..")
+    if (trackLength < 10) {
+      trackLength = 10
+    };
 
-  for (i = 1; i <= trackLength; i++){
-    $("#player1_strip").append("<td>");
-    $("#player2_strip").append("<td>");
-  };
+    for (i = 1; i <= trackLength; i++) {
+      $("#player1_strip").append("<td>")
+      $("#player2_strip").append("<td>")
+    };
+  }; // close setTrackLength
+  setTrackLength()
 
-}; //close setTrackLength
+  alert("Player 1 use 'Q' to move, Player 2 use 'P'.");
 
-setTrackLength();
+  function gameStart () {
+    $("td").removeClass("active") // removes active class from all td's
+    $("#player1_strip > td:first, #player2_strip td:first").addClass("active") // sets first td of both rows to active
 
+    $(document).on('keyup',function(event) { // event listener for keypress
+      if (event.keyCode === 81) {
+        if (player1Move < trackLength) {
+          updatePlayer('player1_strip')
+          player1Move++
+        }
+        else {
+          win("Player 1")
+        }
+      }; // close 'if 81 loop'
 
-alert("Player 1 use 'Q' to move, Player 2 use 'P'.");
+      if (event.keyCode === 80) {
+        if (player2Move < trackLength) {
+          updatePlayer('player2_strip')
+          player2Move++
+        }
+        else {
+          win("Player 2")
+        }
+      }; // close 'if 80 loop'
+      console.log(player1Move, player2Move)
+    }) // close listener
+  } // close 'gameStart function'
+  gameStart() // call 'gameStart' function
 
+  updatePlayer = function (player) {
+    var activeCell = $("#" + player + " td.active")
+    var moveCell = activeCell.next()
 
-function gameStart() {
+    activeCell.removeClass("active")
+    moveCell.addClass("active")
+  } // close 'updatePlayer function'
 
-  $("td").removeClass("active"); //removes active class from all td's
-  $("#player1_strip > td:first, #player2_strip td:first").addClass("active"); //sets first td of both rows to active
+  win = function (winner) {
+    alert(winner + " wins!, Congratulations!")
+    gameOver()
+  } // close 'win function'
 
-  
-  $(document).on('keyup',function(event) { //event listener - keypress
-  
-    if (event.keyCode === 81) {
-      if (player1Move < trackLength) {
-        updatePlayer('player1_strip');
-        player1Move++;
-      }
-      else {
-        win("Player 1");
-      }
-    }; //close 'if 81 loop'
-
-    if (event.keyCode === 80) {
-      if (player2Move < trackLength) {
-        updatePlayer('player2_strip');
-        player2Move++;
-      }
-      else {
-        win("Player 2");
-      }
-    }; //close 'if 80 loop'
-    console.log(player1Move, player2Move);
-
-  }); //close listener
-
-
-}; //close 'gameStart function'
-
-
-gameStart();
-
-  updatePlayer = function(player) {
-    var activeCell = $("#" + player + " td.active");
-    var moveCell = activeCell.next();
-
-    activeCell.removeClass("active");
-    moveCell.addClass("active");
-  }; //close 'updatePlayer function'
-
-  win = function(winner) {
-    alert(winner + " wins!, Congratulations!");
-    gameOver();
-  }; //close 'win function'
-
-  function gameOver() {
-        window.location.reload(true);
-  }; //close 'game over function'
-
-}); //close 'document ready function'
-
+  function gameOver () {
+    window.location.reload(true)
+  }; // close 'game over function'
+}) // close 'document ready function'
