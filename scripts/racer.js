8b845bed8acad1dcced28b4acd2bf0290919faad
jess-of-the-woods <what.the.dickenss@gmail.com>
2016-03-07 22:03:48 +1300

    edit notes, new styles in css, new classes add to td's, comment out code in racer.js for debugging purposes

--- scripts/racer.js
+++ scripts/racer.js
@@ -3,27 +3,32 @@ $(document).ready(function () {
   var player2Move = 0
   var trackLength = 0
 
-// this section sets the length of the track from user input
+  // this section sets the length of the track from user input
 /*  function setTrackLength () {
+    // get user input & if less than 10, set to 10
     trackLength = prompt("Please type a number between 10 & 40 to choose your track length..")
     if (trackLength < 10) {
       trackLength = 10
-    };
+    }; // close if
 
+    // set trackLength = user input (append td's)
     for (i = 1; i <= trackLength; i++) {
-      $("#player1_strip").append("<td>")
+      $("#player1_strip").append("<td class='p1_strip'>")
       $("#player2_strip").append("<td>")
-    };
+    }; // close 'for loop'
   }; // close setTrackLength
   setTrackLength()*/
 
 /*  alert("Player 1 use 'Q' to move, Player 2 use 'P'.");*/
-
+  
   function gameStart () {
-    $("td").removeClass("active") // removes active class from all td's
-    $("#player1_strip > td:first, #player2_strip td:first").addClass("active") // sets first td of both rows to active
+    // removes active class from all td's
+    $("td").removeClass("active")
+    // sets first td of both rows to active
+    $("#p1_strip > td:first, #p2_strip td:first").addClass("active")
 
-    $(document).on('keyup',function(event) { // event listener for keypress
+    // event listener for keypress
+    $(document).on('keyup', function (event) {
       if (event.keyCode === 81) {
         if (player1Move < trackLength) {
           updatePlayer('player1_strip')
@@ -40,28 +45,30 @@ $(document).ready(function () {
           player2Move++
         }
         else {
-          win("Player 2")
+          win('Player 2')
         }
       }; // close 'if 80 loop'
-      console.log(player1Move, player2Move)
+     // console.log(player1Move, player2Move)
     }) // close listener
   } // close 'gameStart function'
   gameStart() // call 'gameStart' function
 
   updatePlayer = function (player) {
-    var activeCell = $("#" + player + " td.active")
+    var activeCell = $('#' + player + ' .active')
     var moveCell = activeCell.next()
 
-    activeCell.removeClass("active")
-    moveCell.addClass("active")
+    activeCell.removeClass('active')
+    moveCell.addClass('active')
   } // close 'updatePlayer function'
 
-  win = function (winner) {
-    alert(winner + " wins!, Congratulations!")
+  // alerts player they have won and calls gameOver function
+ /* var win = function (winner) {
+    alert(winner + ' wins!, Congratulations!')
     gameOver()
-  } // close 'win function'
+  }
 
-  function gameOver () {
+  // reloads game
+  var gameOver = function () {
     window.location.reload(true)
-  }; // close 'game over function'
+  } // close 'gameOver function'*/
 }) // close 'document ready function'
